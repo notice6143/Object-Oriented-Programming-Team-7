f0e0a6bd8a6ee88a22c05b8a5dfadd0b6b970daf
paek jae seoung <notice6143@kau.kr>
2022-11-05 23:59:14 +0900

    Calendar Update

--- app/src/main/java/com/example/myprojectteam7/MainActivity.kt
+++ app/src/main/java/com/example/myprojectteam7/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.myprojectteam7
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myprojectteam7.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
     }
 }
