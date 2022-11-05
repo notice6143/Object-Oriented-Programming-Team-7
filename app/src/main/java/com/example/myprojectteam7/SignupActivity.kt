89196e69698f97a6dfb49ace013182edea5217c3
paek jae seoung <notice6143@kau.kr>
2022-11-06 03:12:50 +0900

    Login Update

--- app/src/main/java/com/example/myprojectteam7/SignupActivity.kt
+++ app/src/main/java/com/example/myprojectteam7/SignupActivity.kt
@@ -4,13 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myprojectteam7.databinding.ActivitySignupBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
 
 class SignupActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignupBinding
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSignup.setOnClickListener {
+            database = Firebase.database.reference
+
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            finish()
+        }
     }
+
 }
