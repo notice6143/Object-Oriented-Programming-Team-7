827bd16f7c5688d952a73cb5a0bc928ba8e41f46
paek jae seoung <notice6143@kau.kr>
2022-11-07 00:25:26 +0900

    Signup Update

--- app/src/main/java/com/example/myprojectteam7/SignupActivity.kt
+++ app/src/main/java/com/example/myprojectteam7/SignupActivity.kt
@@ -11,16 +11,29 @@ import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignupBinding
-    private lateinit var database: DatabaseReference
+    lateinit var database: DatabaseReference
+    lateinit var databasechild: DatabaseReference
+    data class User(val username: String? = null, val usernumber: String? = null, val userpassword: String? = null) {
 
+    }
+    fun writeNewUser(name: String, number: String, password: String) {
+        val user = User(name, number, password)
+        database.child("Users").child(number).setValue(user)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnSignup.setOnClickListener {
+            //database = FirebaseDatabase.getInstance().getReference()
             database = Firebase.database.reference
-
+            databasechild = database.child("User")
+            val username = binding.editName.getText().toString()
+            val usernumber = binding.editPhone.getText().toString()
+            val userpassword = binding.editPw.getText().toString()
+            writeNewUser(username, usernumber, userpassword)
+            //databasechild.push().setValue(username)
             val intent = Intent(this, LoginActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
             startActivity(intent)
