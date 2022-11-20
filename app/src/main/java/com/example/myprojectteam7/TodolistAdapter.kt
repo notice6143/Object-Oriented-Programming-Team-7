181fe5496b9797bd77f954a2bd70695d33e23309
paek jae seoung <notice6143@kau.kr>
2022-11-21 07:08:58 +0900

    221121 todolist, calendar update

---
+++ app/src/main/java/com/example/myprojectteam7/TodolistAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.myprojectteam7
+
+
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myprojectteam7.databinding.ListScheduleBinding
+import java.time.LocalDate
+
+@RequiresApi(Build.VERSION_CODES.O)
+class TodolistAdapter(val myCal:Mycalendar): RecyclerView.Adapter<TodolistAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val binding = ListScheduleBinding.inflate(LayoutInflater.from(parent.context))
+        return Holder(binding)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(myCal.todolist[position], myCal)
+    }
+
+    override fun getItemCount() = myCal.listcount
+
+    class Holder(val binding: ListScheduleBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(list: Todolists?, myCal: Mycalendar) {
+            val index=myCal.didx.indexOf(myCal.day.toInt())
+            binding.txtTitle.text = myCal.tidx[index]
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myprojectteam7/Todolists.kt
@@ -0,0 +1,3 @@
+package com.example.myprojectteam7
+
+data class Todolists(val index: Int, val title: String, val day: Int)
